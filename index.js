412927189f8e2c6e207692185cd4049c1d9c6b3e
Marek Serafin <marek@snowheads.pl>
2025-04-15 19:50:15 +0200

    chore: cleanup

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const withBindings = require('./lib/resolve-bindings');
 
-// Expose both the default instance and the custom binding function
 module.exports = withBindings();
 module.exports.withBindings = withBindings;
